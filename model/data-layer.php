99b3014b9fa6d54fbe30481c75cbaf97e81f962a
Eashune Abenojar <EashuneAbenojar@hotmail.com>
2021-05-16 04:37:18 -0400

    added model file

---
+++ model/data-layer.php
@@ -0,0 +1,12 @@
+<?php
+
+/* data-layer.php
+ * Return data for the diner app
+ *
+ */
+
+// Get the Choices
+function getChoices()
+{
+    return array("This midterm is easy", "I like midterms", "Today is Monday");
+}
